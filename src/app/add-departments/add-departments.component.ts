8046b48857e1e110ea12cb523e9867aa5e9baf12
Iminlove24042014 <malazafanelesibonge@gmail.com>
2022-12-17 22:43:30 +0200

    create add departments files

---
+++ src/app/add-departments/add-departments.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-departments',
+  templateUrl: './add-departments.component.html',
+  styleUrls: ['./add-departments.component.sass']
+})
+export class AddDepartmentsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
