a7df4654307be51f5aa6b7cfa214ac374b97980f
TheFaro <malazafanelesibonge@gmail.com>
2023-02-07 18:16:07 +0200

    finalize before switching to material theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+// import { hammerjs } from '../../node_modules/hammerjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.sass'],
 })
 export class AppComponent {
+  // hammerjs = hammerjs;
   title = 'school-management-system-SMS';
 }
