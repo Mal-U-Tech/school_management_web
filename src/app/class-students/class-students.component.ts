a6e8bbbe316853abe8aaabc8da2cda040dfcad43
TheFaro <malazafanelesibonge@gmail.com>
2023-04-04 05:29:49 +0200

    create new class-student component

---
+++ src/app/class-students/class-students.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-class-students',
+  templateUrl: './class-students.component.html',
+  styleUrls: ['./class-students.component.scss']
+})
+export class ClassStudentsComponent {
+
+}
