abf0613ad00297787f11dd741c884c5cbda29cdf
TheFaro <malazafanelesibonge@gmail.com>
2023-04-12 19:12:36 +0200

    SMS-11 create view table component

---
+++ src/app/subject-teacher/view-sub-teacher-table/view-sub-teacher-table.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-sub-teacher-table',
+  templateUrl: './view-sub-teacher-table.component.html',
+  styleUrls: ['./view-sub-teacher-table.component.scss']
+})
+export class ViewSubTeacherTableComponent {
+
+}
