6f601ec46b01fa235475f47a10fdec911b021537
TheFaro <malazafanelesibonge@gmail.com>
2023-04-14 09:47:46 +0200

    SMS-3 add view-class-teacher component to app module

---
+++ src/app/class-teacher/view-class-teacher-table/view-class-teacher-table.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-class-teacher-table',
+  templateUrl: './view-class-teacher-table.component.html',
+  styleUrls: ['./view-class-teacher-table.component.scss']
+})
+export class ViewClassTeacherTableComponent {
+
+}
