b08e21006930da6e1056e7c6ec764cd49d0d1992
TheFaro <malazafanelesibonge@gmail.com>
2023-04-19 16:42:57 +0200

    SMS-52 added scoresheet creation components

---
+++ src/app/scoresheet/create-scoresheet/create-scoresheet/create-scoresheet.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-scoresheet',
+  templateUrl: './create-scoresheet.component.html',
+  styleUrls: ['./create-scoresheet.component.scss'],
+})
+export class CreateScoresheetComponent {
+  public name = '';
+  public year = '';
+}
