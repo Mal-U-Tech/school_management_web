450983ddbacc84a9e6e01be6d681a308fda55945
TheFaro <malazafanelesibonge@gmail.com>
2023-04-21 23:24:13 +0200

    SMS-52 create scoresheet details component (everything)

--- src/app/scoresheet/create-scoresheet/details/details.component.ts
+++ src/app/scoresheet/create-scoresheet/details/details.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ScoresheetService } from 'src/app/shared/scoresheet/scoresheet.service';
 
 @Component({
-  selector: 'app-details',
+  selector: 'scoresheet-details',
   templateUrl: './details.component.html',
-  styleUrls: ['./details.component.scss']
+  styleUrls: ['./details.component.scss'],
 })
 export class DetailsComponent {
+  constructor(public scoresheetService: ScoresheetService) {}
 
+  goToNextStep() {
+    // remove first step
+    // display second step
+    if (this.scoresheetService.isStepOne) {
+      this.scoresheetService.isStepOne = false;
+      this.scoresheetService.isStepTwo = true;
+    }
+  }
 }
