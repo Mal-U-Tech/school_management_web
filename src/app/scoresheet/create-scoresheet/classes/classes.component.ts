f46cc3c40cb00418c5c9dfe8286cd07a8ae2273a
TheFaro <malazafanelesibonge@gmail.com>
2023-04-21 23:21:27 +0200

    SMS-52 create scoresheet classes component(everything)

--- src/app/scoresheet/create-scoresheet/classes/classes.component.ts
+++ src/app/scoresheet/create-scoresheet/classes/classes.component.ts
@@ -1,10 +1,76 @@
 import { Component } from '@angular/core';
+import { ScoresheetService } from 'src/app/shared/scoresheet/scoresheet.service';
 
 @Component({
-  selector: 'app-classes',
+  selector: 'scoresheet-classes',
   templateUrl: './classes.component.html',
-  styleUrls: ['./classes.component.scss']
+  styleUrls: ['./classes.component.scss'],
 })
 export class ClassesComponent {
+  public classes: any;
 
+  constructor(public scoresheetService: ScoresheetService) {}
+
+  ngOnInit(): void {
+    this.getClasses();
+    this.computeCheckedClassesArray();
+    this.getSubjects();
+  }
+
+  // function to retrieve classes from sessionStorage
+  getClasses() {
+    this.classes = JSON.parse(sessionStorage.getItem('streams')!);
+  }
+
+  // function to retrieve subjects from sessionStorage
+  getSubjects() {
+    this.scoresheetService.subjects = JSON.parse(
+      sessionStorage.getItem('subjects')!
+    );
+  }
+
+  // function to create checked variable for the number of classes available
+  computeCheckedClassesArray() {
+    let temp: boolean[] = [];
+
+    for (let i = 0; i < this.classes.length; i++) {
+      temp.push(false);
+    }
+
+    this.checkIfCheckedExists(temp);
+  }
+
+  checkIfCheckedExists(temp: boolean[]) {
+    if (!this.scoresheetService.checked.length) {
+      this.scoresheetService.checked = temp;
+    }
+  }
+
+  createScoresheet() {
+    console.log(this.scoresheetService.name);
+    console.log(this.scoresheetService.year);
+    console.log(this.scoresheetService.checked);
+    console.log(this.scoresheetService.subjects);
+    this.assignCheckedClasses();
+    console.log(this.scoresheetService.selectedClasses);
+  }
+
+  assignCheckedClasses() {
+    let temp: object[] = [];
+    for (let i = 0; i < this.scoresheetService.checked.length; i++) {
+      let val = this.scoresheetService.checked[i];
+
+      if (val) {
+        // this class has been selected
+        temp.push(this.classes[i]);
+      }
+    }
+
+    this.scoresheetService.selectedClasses = temp;
+  }
+
+  goBackToStepOne() {
+    this.scoresheetService.isStepTwo = false;
+    this.scoresheetService.isStepOne = true;
+  }
 }
