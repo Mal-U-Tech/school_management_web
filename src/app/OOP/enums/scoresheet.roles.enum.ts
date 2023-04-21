4a0e3cac191846ba194d1ee839e4862be75962ee
TheFaro <malazafanelesibonge@gmail.com>
2023-04-21 23:30:02 +0200

    SMS-52 created enum types for scoresheet and user roles

---
+++ src/app/OOP/enums/scoresheet.roles.enum.ts
@@ -0,0 +1,8 @@
+export enum ScoresheetRolesEnum {
+  administrator = 'Administrator',
+  teacher = 'Teacher',
+  headteacher = 'Headteacher',
+  head_of_department = 'Head Of Department',
+  deputy = 'Deputy Headteacher',
+  none = 'None',
+}
