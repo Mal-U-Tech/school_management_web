1b40e34dbf8d6d19c90966a32b9bd55cf803258a
TheFaro <malazafanelesibonge@gmail.com>
2023-04-21 23:23:13 +0200

    SMS-52 add components to create scoresheet: details and classes

--- src/app/scoresheet/create-scoresheet/create-scoresheet/create-scoresheet.component.ts
+++ src/app/scoresheet/create-scoresheet/create-scoresheet/create-scoresheet.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Scoresheet } from 'src/app/OOP/classes/scoresheet.class';
+import { ScoresheetService } from 'src/app/shared/scoresheet/scoresheet.service';
 
 @Component({
   selector: 'app-create-scoresheet',
@@ -6,6 +8,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./create-scoresheet.component.scss'],
 })
 export class CreateScoresheetComponent {
-  public name = '';
-  public year = '';
+  constructor(public scoresheetService: ScoresheetService) {}
+
+  ngOnInit(): void {
+    this.scoresheetService.isStepTwo = false;
+    this.scoresheetService.isStepOne = true;
+  }
+
+  // declare a new scoresheet object
+  public scoresheet: Scoresheet = new Scoresheet({
+    api: this.scoresheetService,
+    year: '2022',
+  });
+
+  ngOnDestroy(): void {
+    this.scoresheetService.checked = [];
+    this.scoresheetService.selectedClasses = [];
+    this.scoresheetService.name = '';
+    this.scoresheetService.year = '';
+  }
 }
