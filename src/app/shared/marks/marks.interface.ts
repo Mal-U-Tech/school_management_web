e855ddd1b5f3b0b23b67b553dfe5df6a3326911b
TheFaro <malazafanelesibonge@gmail.com>
2023-05-04 20:13:37 +0200

    SMS-52 create marks end point methods

---
+++ src/app/shared/marks/marks.interface.ts
@@ -0,0 +1,9 @@
+export interface MarksInterface {
+  class_student_id: string;
+  subject_teacher_id: string;
+  subject_id: string;
+  mark: string;
+  year: string;
+  scoresheet_id: string;
+  max_score: number;
+}
