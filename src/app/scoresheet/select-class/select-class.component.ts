0570cf5038ecc765d894035bacbe839f47f5e351
TheFaro <malazafanelesibonge@gmail.com>
2023-05-04 19:59:55 +0200

    SMS-52 add action row to navigate to view scoresheet

--- src/app/scoresheet/select-class/select-class.component.ts
+++ src/app/scoresheet/select-class/select-class.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { AddSubjectsService } from 'src/app/shared/add-subjects/add-subjects.service';
+import { MarksService } from 'src/app/shared/marks/marks.service';
 import { ScoresheetService } from 'src/app/shared/scoresheet/scoresheet.service';
 import { SubjectTeacherService } from 'src/app/shared/subject-teacher/subject-teacher.service';
 
@@ -12,7 +14,9 @@ export class SelectClassComponent {
   constructor(
     public service: ScoresheetService,
     public subjects: AddSubjectsService,
-    public subjectTeacher: SubjectTeacherService
+    public subjectTeacher: SubjectTeacherService,
+    public router: Router,
+    public marksService: MarksService
   ) {}
 
   ngAfterViewInit(): void {
@@ -20,10 +24,6 @@ export class SelectClassComponent {
       .getStreamsFromScoresheet(this.service.selectedScoresheetId)
       .subscribe({
         next: (data: any) => {
-          console.log(data[0].classes);
-          console.log(this.secondaryRegEx.test(data[0].classes[1].name));
-          console.log(data[0].classes[1].name);
-
           for (let i = 0; i < data[0].classes.length; i++) {
             let temp = data[0].classes[i];
 
@@ -65,18 +65,55 @@ export class SelectClassComponent {
     this.selectedClass--;
   }
 
-  checkTeacher(subjectId: string, classId: string, year: string) {
-    let teacherId = '6442e23f66c6b3a6650b0f02';
+  checkTeacher(subjectId: string, classId: string) {
+    let currentUser = JSON.parse(sessionStorage.getItem('user')!);
+    console.log(currentUser);
+    // let teacherId = '6442e23f66c6b3a6650b0f02';
     this.subjectTeacher
       .getTeacherForSubject(
         subjectId,
-        teacherId,
+        currentUser.teacher_id,
         classId,
         this.service.selectedYear
       )
       .subscribe({
         next: (data: any) => {
           console.log(data);
+          sessionStorage.setItem(
+            'selected-class-scoresheet',
+            JSON.stringify(data)
+          );
+
+          this.subjectTeacher.successToast('Successfully retrieved students');
+          this.router.navigateByUrl('add-marks');
+        },
+        error: (error) => {
+          console.log(error);
+          this.subjectTeacher.errorToast(
+            'Please correctly choose the class you teach'
+          );
+        },
+      });
+  }
+
+  // function to take user to the full scoresheet view of the class
+  viewScoresheet(stream: any) {
+    console.log(stream);
+    console.log(this.service.selectedScoresheetId);
+
+    this.marksService
+      .getFullScoresheet(stream.class_id, this.service.selectedScoresheetId)
+      .subscribe({
+        next: (data: any) => {
+          console.log(data);
+          this.marksService.selectedClass = {
+            class_id: stream.class_id,
+            name: stream.name,
+          };
+
+          sessionStorage.setItem('scoresheet-data', JSON.stringify(data));
+          // navigate to class scoresheet component
+          this.router.navigateByUrl('class-scoresheet');
         },
         error: (error) => {
           console.log(error);
