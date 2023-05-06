92737ee02308a440cf1fc2218492024b3a513da6
Iminlove24042014 <malazafanelesibonge@gmail.com>
2023-05-06 09:14:22 +0200

    SMS-69 create pass controls component

---
+++ src/app/pass-controls/pass-controls.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pass-controls',
+  templateUrl: './pass-controls.component.html',
+  styleUrls: ['./pass-controls.component.scss']
+})
+export class PassControlsComponent {
+
+}
