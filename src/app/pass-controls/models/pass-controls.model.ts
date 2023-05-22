a31a6540deef7bdafc0917cc79cc9f669993596a
Fanelesibonge Malaza <malazafanelesibonge@gmail.com>
2023-05-22 23:08:43 +0200

    SMS-77 remove passing_subject object. Causing error in serverless

--- src/app/pass-controls/models/pass-controls.model.ts
+++ src/app/pass-controls/models/pass-controls.model.ts
@@ -1,10 +1,8 @@
 export interface IPassControls {
   _id?: string;
   scoresheet_id: string;
-  passing_subject: {
-    name: string;
-    mark: number;
-  };
+  name: string;
+  mark: number;
   aggregate: number;
   num_passed_subject: number;
   other_subjects_mark: number;
