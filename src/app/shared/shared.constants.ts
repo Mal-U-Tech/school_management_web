d56ea0bc33a7c727cf40f9396aac7f498d530120
Fanelesibonge Malaza <malazafanelesibonge@gmail.com>
2023-05-22 18:15:03 +0200

    SMS-72 update api endpoint url

--- src/app/shared/shared.constants.ts
+++ src/app/shared/shared.constants.ts
@@ -4,7 +4,7 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 export abstract class SharedApiConstants {
   constructor(private snackBar: MatSnackBar) {}
   // apiUrl = 'http://localhost:3000/dev/';
-  apiUrl = '/dev/';
+  apiUrl = 'https://alh2fmir3d.execute-api.us-east-1.amazonaws.com/dev/';
   httpOptions = {
     headers: new HttpHeaders({
       'Content-Type': 'application/json',
