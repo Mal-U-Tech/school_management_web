9b949e48cda88478136ffbff3428b62ebbdfb8e2
Fanelesibonge Malaza <malazafanelesibonge@gmail.com>
2023-06-22 19:08:40 +0200

    SMS-88 add store and actions

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -10,6 +10,19 @@ import { Router } from '@angular/router';
 import { ErrorStateMatcher } from '@angular/material/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { IUser } from '../shared/user/user.interface';
+import { Store } from '@ngrx/store';
+import { isLoading, login } from '../store/user/user.actions';
+import {
+  selectIsLoading,
+  selectToken,
+  selectUserData,
+} from '../store/user/user.selector';
+import {
+  checkModulesRequest,
+  isSchoolInfoLoading,
+} from '../store/school-info/school-info.actions';
+import { selectSchoolInfoIsLoading } from '../store/school-info/school-info.selector';
+import { tap } from 'rxjs';
 
 export class MyErrorStateMatcher implements ErrorStateMatcher {
   isErrorState(
@@ -31,7 +44,12 @@ export class MyErrorStateMatcher implements ErrorStateMatcher {
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
-  public snackbarMessage = 'This is a snackbar message';
+  constructor(
+    public userApi: UserApiService,
+    public router: Router,
+    private _snackBar: MatSnackBar,
+    private store: Store
+  ) {}
   /*
    *  Form control variable : Reactive Forms
    * */
@@ -47,68 +65,98 @@ export class LoginComponent {
     opacity: '1',
     display: 'block',
   };
-  isLoading = false;
-  isCheckModulesLoading = false;
+  isLoading$ = this.store.select(selectIsLoading);
+  isSchoolInfoLoading$ = this.store.select(selectSchoolInfoIsLoading);
 
-  constructor(
-    public userApi: UserApiService,
-    public router: Router,
-    private _snackBar: MatSnackBar
-  ) {}
+  public user$ = this.store.select(selectUserData);
+  public token$ = this.store.select(selectToken);
 
   public submitForm() {
-    this.isLoading = !this.isLoading;
-    this.userApi
-      .userLogin(this.email.value as string, this.password.value as string)
-      .subscribe({
-        next: (data: IUser) => {
-          // TODO: route to home, once system is done
-          console.log(data);
-          this.isLoading = false;
+    this.store.dispatch(
+      login({
+        email: this.email.value as string,
+        password: this.password.value as string,
+      })
+    );
 
-          this.spinner = {
-            opacity: '1',
-            display: 'block',
-          };
-          // save user data for
-          sessionStorage.setItem('userData', JSON.stringify(data));
-          this.checkModules(data);
-        },
-        error: (error) => {
-          this.isLoading = false;
-          console.log(`This is error: ${error}`);
-          this.userApi.errorToast(error);
+    this.user$
+      .subscribe({
+        next: (data) => {
+          if (data == null) {
+            console.log('I am null');
+          } else {
+            console.log(`This is where I am: ${data}`);
+          }
         },
       });
-  }
 
-  public checkModules(data: IUser) {
-    this.isCheckModulesLoading = !this.isCheckModulesLoading;
-    this.userApi.checkModules(data._id || '').subscribe({
-      next: (res) => {
-        this.isCheckModulesLoading = false;
-        sessionStorage.setItem('school-info', JSON.stringify(res.data));
-        console.log(res);
-        if (res.missing.length) {
-          const missing = res.missing[0].name;
-
-          if (missing === 'school-info') {
-            sessionStorage.setItem('user', JSON.stringify(data));
-            this.router.navigate([`/school-registration`]);
-          }
-        }
-
-        if (res.success === 100) {
-          this.router.navigate(['/dashboard/academics']);
-        }
-      },
-      error: (error) => {
-        this.isCheckModulesLoading = false;
-        console.log(error);
-        this.userApi.errorToast(error.toString());
+    this.token$.subscribe({
+      next: (data: any) => {
+        console.log(`This is token ${data}`);
       },
     });
+
+    // dispatch action to load
+    this.isLoadingUser();
+    // this.userApi
+    //   .userLogin(this.email.value as string, this.password.value as string)
+    //   .subscribe({
+    //     next: (data: IUser) => {
+    //       // TODO: route to home, once system is done
+    //       console.log(data);
+    //       this.isLoading = false;
+    //
+    //       this.spinner = {
+    //         opacity: '1',
+    //         display: 'block',
+    //       };
+    //       // save user data for
+    //       sessionStorage.setItem('userData', JSON.stringify(data));
+    //       this.checkModules(data);
+    //     },
+    //     error: (error) => {
+    //       this.isLoading = false;
+    //       console.log(`This is error: ${error}`);
+    //       this.userApi.errorToast(error);
+    //     },
+    //   });
   }
+
+  isLoadingUser() {
+    this.store.dispatch(isLoading({ isLoading: true }));
+  }
+
+  // public checkModules(data: IUser) {
+  //   // this.isCheckModulesLoading = !this.isCheckModulesLoading;
+  //   this.isLoadingSchoolInfoAction();
+  //
+  //   // dispatch action to get school
+  //   // this.store.dispatch(checkModulesRequest())
+  //   this.userApi.checkModules(data._id || '').subscribe({
+  //     next: (res) => {
+  //       this.isCheckModulesLoading = false;
+  //       sessionStorage.setItem('school-info', JSON.stringify(res.data));
+  //       console.log(res);
+  //       if (res.missing.length) {
+  //         const missing = res.missing[0].name;
+  //
+  //         if (missing === 'school-info') {
+  //           sessionStorage.setItem('user', JSON.stringify(data));
+  //           this.router.navigate([`/school-registration`]);
+  //         }
+  //       }
+  //
+  //       if (res.success === 100) {
+  //         this.router.navigate(['/dashboard/academics']);
+  //       }
+  //     },
+  //     error: (error) => {
+  //       this.isCheckModulesLoading = false;
+  //       console.log(error);
+  //       this.userApi.errorToast(error.toString());
+  //     },
+  //   });
+  // }
 
   public navToRegister() {
     this.router.navigate([`/registration`]);
