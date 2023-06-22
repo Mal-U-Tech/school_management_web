ba6093a300acd12793e5bc190f729a92d858d9b9
Fanelesibonge Malaza <malazafanelesibonge@gmail.com>
2023-06-22 19:09:54 +0200

    SMS-88 update app url to local environment

--- src/app/shared/shared.constants.ts
+++ src/app/shared/shared.constants.ts
@@ -2,8 +2,8 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 
 export abstract class SharedApiConstants {
   constructor(private snackBar: MatSnackBar) {}
-  // apiUrl = 'http://localhost:3000/dev/';
-  apiUrl = 'https://woak4feg7f.execute-api.us-east-1.amazonaws.com/dev/';
+  apiUrl = 'http://localhost:3000/dev/';
+  // apiUrl = 'https://woak4feg7f.execute-api.us-east-1.amazonaws.com/dev/';
 
   successToast(message: string) {
     this.snackBar.open(message, 'Close', { duration: 3000 });
