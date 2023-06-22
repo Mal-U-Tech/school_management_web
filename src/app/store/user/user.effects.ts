b99856bdd8bc10e7bf905170b6725e14a60a2298
Fanelesibonge Malaza <malazafanelesibonge@gmail.com>
2023-06-22 19:14:13 +0200

    SMS-88 add user ngrx

--- src/app/store/user/user.effects.ts
+++ src/app/store/user/user.effects.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
-import { createEffect, ofType } from '@ngrx/effects';
-import { mergeMap } from 'rxjs';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { catchError, from, map, mergeMap, of, switchMap, tap } from 'rxjs';
 import { UserApiService } from 'src/app/shared/user/user-api.service';
+import { IUser } from 'src/app/shared/user/user.interface';
+import {
+  checkModulesRequest,
+  isSchoolInfoLoading,
+} from '../school-info/school-info.actions';
+import {
+  isLoading,
+  login,
+  loginError,
+  loginSuccessful,
+  setToken,
+  setUser,
+} from './user.actions';
 
 @Injectable()
 export class AuthEffects {
@@ -11,9 +24,65 @@ export class AuthEffects {
   login$ = createEffect(() => {
     return this.actions$.pipe(
       ofType(login),
-      mergeMap(({ email, password }) => {
-        return this.authService.userLogin(email, password);
-      })
+      mergeMap(({ email, password }) =>
+        from(this.authService.authLogin(email, password)).pipe(
+          tap(({ token }) => {
+            this.authService.successToast('Login successful.');
+            return setToken({ token });
+          }),
+          // map(({ user }) => setUser({user: user as IUser})),
+          catchError((error) => {
+            this.authService.errorToast(error);
+            return of(loginError({ message: error.toString() }));
+          })
+        )
+      ),
+      // switchMap((user) => {
+      //   // console.log(user as IUser);
+      //   // assign values to new user
+      //   const currUser = user as IUser;
+      //   of(setUser({ user: currUser }));
+      //   of(setToken({ token: currUser.token }));
+      //   isLoading({ isLoading: false });
+      //
+      //   // dispatch action to load school info from the server
+      //   return of(
+      //     checkModulesRequest({ _id: currUser._id || '', user: currUser })
+      //   );
+      // })
+      switchMap(
+        (user) =>
+          of(
+            isLoading({ isLoading: false }),
+            setUser({ user: user as IUser }),
+            setToken({ token: (user as IUser).token }),
+            isSchoolInfoLoading({ isSchoolInfoLoading: true }),
+            checkModulesRequest({
+              _id: (user as IUser)._id || '',
+              user: user as IUser,
+            })
+          )
+
+        //   from(user).pipe(
+        //     tap(({ user }) => setUser({ user: user as IUser })),
+        //     tap(({ user }) => setToken({ token: (user as IUser).token })),
+        //     tap(({ user }) => {
+        //       const currUser = user as IUser;
+        //       if (currUser.token != null || currUser.token != '') {
+        //         return of(isLoading({ isLoading: false }));
+        //       }
+        //
+        //       return of(
+        //         loginSuccessful({
+        //           user: currUser,
+        //           isLoading: false,
+        //         })
+        //       );
+        //     })
+        //   )
+        // )
+        // switchMap(() => checkModulesRequest({ _id: (user as IUser)._id || '', user: user as IUser }),
+      )
     );
   });
 
--- src/app/store/user/user.states.ts
+++ src/app/store/user/user.states.ts
@@ -1,9 +1,17 @@
+import { ISchoolInfo } from 'src/app/school-registration/models/school-info.model';
 import { IUser } from 'src/app/shared/user/user.interface';
 
 export interface AuthState {
-  user: IUser | null;
+  token: string;
+  user: IUser;
+  status: 'pending' | 'loading' | 'error' | 'success';
+  error?: string;
+  isLoginProgress: boolean;
 }
 
 export const initialState: AuthState = {
-  user: null,
+  token: '',
+  user: null as any,
+  status: 'pending',
+  isLoginProgress: false,
 };
