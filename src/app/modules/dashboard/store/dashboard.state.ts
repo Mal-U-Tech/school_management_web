65e2f1022c57f2034a6f72a2cd8cfa0ae7063c2e
ashwynh21 <ashwynh21@gmail.com>
2023-09-22 13:15:16 +0200

    feat(dashboard): optimize the look of the drawer

--- src/app/modules/dashboard/store/dashboard.state.ts
+++ src/app/modules/dashboard/store/dashboard.state.ts
@@ -1,10 +1,30 @@
-import { IMenu } from "../interfaces/menu.interface";
+import { IMenu } from '../interfaces/menu.interface';
 
 export interface DashboardState {
-  menu: IMenu[]
+  menu: IMenu[];
 }
 
-export const initial: DashboardState = {
-  menu: []
-};
+/*
+const testmenu = {
+  title: 'Home',
+  icon: 'home',
+  route: [''],
 
+  items: [
+    {
+      title: 'test',
+      icon: 'person',
+      route: ['test'],
+    },
+    {
+      title: 'Classes',
+      icon: 'class',
+      route: ['classes']
+    }
+  ],
+};
+*/
+
+export const initial: DashboardState = {
+  menu: [],
+};
