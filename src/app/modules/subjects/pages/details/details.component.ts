c3f880d5f8857587447b93187369d5527923cc3f
justin horton <justinh1804@gmail.com>
2024-01-05 10:47:26 +0200

    sms-100

--- src/app/modules/subjects/pages/details/details.component.ts
+++ src/app/modules/subjects/pages/details/details.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { selectCurrentSchoolSubjects } from 'src/app/modules/schools/store/schools.selectors';
 
 @Component({
   selector: 'app-details',
@@ -6,5 +8,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./details.component.scss']
 })
 export class DetailsComponent {
-
+  subjects$ = this.store.select(selectCurrentSchoolSubjects);
+  constructor(private store : Store){}
 }
--- src/app/modules/subjects/subjects.module.ts
+++ src/app/modules/subjects/subjects.module.ts
@@ -2,6 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DetailsComponent } from './pages/details/details.component';
 import { RouterModule } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { ComponentsModule } from 'src/app/components/components.module';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatTableModule } from '@angular/material/table';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 import routes from './subjects.routes';
 
@@ -12,6 +23,22 @@ import routes from './subjects.routes';
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
-  ]
+
+
+    // internal components
+    ComponentsModule,
+
+    // material imports
+    MatButtonModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatMenuModule,
+    MatTableModule,
+    MatTooltipModule,
+    MatPaginatorModule,
+    MatProgressSpinnerModule,
+  ],
 })
 export class SubjectsModule { }
